de76e6467ce19ebb522ea89244f8aa3bbbb317f1
Ed Sweeney <ed@onextent.com>
2023-03-04 06:44:40 -0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+gen test data for navactor
+==============
+
+Data will eventually be signal k format and generate a graph of marine
+reporters reporting their own info as well as their neighbors.
+
+
+see: https://signalk.org/specification/1.7.0/doc/data_model.html
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='signal_k_gen',
+    version='0.1.0',
+    description='generate test json data based on signal k spec',
+    author='Ed Sweeney',
+    author_email='ed@onextent.com',
+    url='https://github.com/navicore/signal_k_gen',
+    packages=find_packages(),
+)
