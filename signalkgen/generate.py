24ec75ce6507f67d4626dab30f547c86db408661
Ed Sweeney <ed@onextent.com>
2023-03-11 06:46:03 -0800

    improved signalk formatting

--- signalkgen/generate.py
+++ signalkgen/generate.py
@@ -4,50 +4,53 @@ gen signal k json for testing the navactor graph features
 """
 import random
 import math
+import uuid
+from datetime import datetime
 
 
 def generate(num_boats, base_coords, nautical_miles):
     """
     gen signal k json
     """
-    signal_k_data = {
-        "@context": "https://signalk.org/specification/1.4.0/context.json",
-        "vessels": {
-            "boat-1": {
-                "name": "Boat 1",
-                "mmsi": "123456789",
-                "navigation": {
-                    "position": {
-                        "latitude": base_coords[0],
-                        "longitude": base_coords[1]
-                    }
-                },
-                "heading": {
-                    "trueHeading": 0.0
-                },
-                "speedOverGround": 0.0,
-                "courseOverGroundTrue": 0.0
-            }
-        }
-    }
-    for i in range(2, num_boats + 1):
+    vessels = {}
+    for i in range(1, num_boats + 1):
         boat_data = {
             "name": f"Boat {i}",
+            "uuid": f"urn:mrn:signalk:uuid:{str(uuid.uuid4())}",
             "mmsi": str(123456789 + i),
             "navigation": {
                 "position": {
-                    "latitude": base_coords[0] + (random.random() * 2 - 1) *
-                    (nautical_miles / 60),
-                    "longitude": base_coords[1] + (random.random() * 2 - 1) *
-                    (nautical_miles / 60) / math.cos(base_coords[0] *
-                                                     math.pi / 180)
+                    "value": {
+                        "latitude": base_coords[0] + (random.random() * 2 - 1) *
+                        (nautical_miles / 60),
+                        "longitude": base_coords[1] + (random.random() * 2 - 1) *
+                        (nautical_miles / 60) / math.cos(base_coords[0] *
+                                                         math.pi / 180),
+                        "altitude": 0.0
+                    },
+                    "$source": "self",
+                    "timestamp": datetime
+                    .utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
                 }
             },
-            "heading": {
-                "trueHeading": 0.0
+            "headingMagnetic": {
+                "value": 0.0,
+                "$source": "self",
+                "timestamp": datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
             },
-            "speedOverGround": 0.0,
-            "courseOverGroundTrue": 0.0
+            "speedOverGround": {
+                "value": 0.0,
+                "$source": "self",
+                "timestamp": datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+            },
+            "courseOverGroundTrue": {
+                "value": 0.0,
+                "$source": "self",
+                "timestamp": datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+            }
         }
-        signal_k_data["vessels"][f"boat-{i}"] = boat_data
-    return signal_k_data
+        vessels[f"urn:mrn:signalk:uuid:{boat_data['uuid']}"] = boat_data
+    return {"vessels": vessels}
--- signalkgen/move_boats.py
+++ signalkgen/move_boats.py
@@ -11,23 +11,31 @@ def move_boats(signal_k_data):
     """
     boats should move along a vector
     """
-    for boat in signal_k_data["vessels"].values():
+    vessels = signal_k_data["vessels"]
+    for vessel_id in vessels:
+        boat = vessels[vessel_id]
         # calculate new position based on current position, course over ground
         # true, and speed over ground
-        lat = boat["navigation"]["position"]["latitude"]
-        lon = boat["navigation"]["position"]["longitude"]
-        cog_true = boat["courseOverGroundTrue"]
-        sog = boat["speedOverGround"]
+        lat = boat["navigation"]["position"]["value"]["latitude"]
+        lon = boat["navigation"]["position"]["value"]["longitude"]
+        cog_true = boat["courseOverGroundTrue"]["value"]
+        sog = boat["speedOverGround"]["value"]
         lat += (sog / 60) * math.cos(cog_true * math.pi / 180)
         lon += (sog / 60) * math.sin(cog_true *
                                      math.pi / 180) / math.cos(lat *
                                                                math.pi / 180)
         # update boat position, heading, and speed
-        boat["navigation"]["position"]["latitude"] = lat
-        boat["navigation"]["position"]["longitude"] = lon
-        boat["heading"]["trueHeading"] = random.uniform(0, 360)
-        boat["speedOverGround"] = random.randint(1, 15)
-        boat["courseOverGroundTrue"] = boat["heading"]["trueHeading"]
-    signal_k_data["@timestamp"] = datetime.utcnow().strftime(
-            '%Y-%m-%dT%H:%M:%S.%fZ')
-    return signal_k_data
+        boat["navigation"]["position"]["value"]["latitude"] = lat
+        boat["navigation"]["position"]["value"]["longitude"] = lon
+        boat["headingMagnetic"]["value"] = random.uniform(0, 360)
+        boat["speedOverGround"]["value"] = random.randint(1, 15)
+        boat["courseOverGroundTrue"]["value"] = boat["headingMagnetic"]["value"]
+        boat["navigation"]["position"]["timestamp"] = datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+        boat["headingMagnetic"]["timestamp"] = datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+        boat["speedOverGround"]["timestamp"] = datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+        boat["courseOverGroundTrue"]["timestamp"] = datetime.utcnow().strftime(
+                    '%Y-%m-%dT%H:%M:%S.%fZ')
+    return {"vessels": vessels}
